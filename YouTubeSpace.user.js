29aba312cec42938632338a457fb9f947db883cd
Flavius Cosmin <highstrike@gmail.com>
2024-03-09 22:45:21 +0200

    update userscript block

--- YouTubeSpace.user.js
+++ YouTubeSpace.user.js
@@ -1,13 +1,17 @@
 // ==UserScript==
 // @name         YouTube Space
+// @namespace    https://github.com/highstrike
+// @homepageURL  https://github.com/highstrike/youtube-space
 // @description  Fix space bar behaviour when video element is out of focus.
 // @icon         https://raw.githubusercontent.com/highstrike/youtube-space/master/icon.png
-// @namespace    https://github.com/highstrike/youtube-space
-// @version      1.0
 // @author       highstrike
-// @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @match        *://www.youtube.com/*
 // @grant        none
+// @version      1.0.1
+// @copyright    2024, highstrike (https://openuserjs.org/users/highstrike)
+// @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
+// @updateURL    https://openuserjs.org/meta/highstrike/YouTube_Space.meta.js
+// @downloadURL  https://openuserjs.org/install/highstrike/YouTube_Space.user.js
 // ==/UserScript==
 
 window.addEventListener('keydown', function(e) {
